1ae59307fa01ff7e07c800141f0c24faa5c0f70a
Martin Visek <weephy@gmail.com>
2026-08-11 23:05:04 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'suchless-v1';
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(['./index.html', './manifest.json']))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
